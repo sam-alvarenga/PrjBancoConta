d5eebd789624908da80d379b290b2be92b6e7ba7
Samantha <samantha.c.alvarenga@gmail.com>
2024-11-08 22:28:44 -0300

    explicação de localização e monétario

---
+++ PrjBancoConta/Demo/frmDemo.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace PrjBancoConta.Forms.Demo
+{
+    partial class frmDemo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtNumBruto = new TextBox();
+            txtNumFormatado = new TextBox();
+            btnFormatar = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtNumFormatado2 = new TextBox();
+            SuspendLayout();
+            // 
+            // txtNumBruto
+            // 
+            txtNumBruto.Location = new Point(119, 99);
+            txtNumBruto.Name = "txtNumBruto";
+            txtNumBruto.Size = new Size(321, 23);
+            txtNumBruto.TabIndex = 0;
+            // 
+            // txtNumFormatado
+            // 
+            txtNumFormatado.Location = new Point(119, 183);
+            txtNumFormatado.Name = "txtNumFormatado";
+            txtNumFormatado.Size = new Size(321, 23);
+            txtNumFormatado.TabIndex = 1;
+            
+            // 
+            // btnFormatar
+            // 
+            btnFormatar.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnFormatar.Location = new Point(346, 318);
+            btnFormatar.Name = "btnFormatar";
+            btnFormatar.Size = new Size(94, 34);
+            btnFormatar.TabIndex = 2;
+            btnFormatar.Text = "Formatar";
+            btnFormatar.UseVisualStyleBackColor = true;
+            btnFormatar.Click += btnFormatar_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(119, 76);
+            label1.Name = "label1";
+            label1.Size = new Size(103, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Número Bruto";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(119, 160);
+            label2.Name = "label2";
+            label2.Size = new Size(140, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Número Formatado";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(119, 230);
+            label3.Name = "label3";
+            label3.Size = new Size(140, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Número Formatado";
+            // 
+            // txtNumFormatado2
+            // 
+            txtNumFormatado2.Location = new Point(119, 265);
+            txtNumFormatado2.Name = "txtNumFormatado2";
+            txtNumFormatado2.Size = new Size(321, 23);
+            txtNumFormatado2.TabIndex = 5;
+            // 
+            // frmDemo
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(569, 375);
+            Controls.Add(label3);
+            Controls.Add(txtNumFormatado2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnFormatar);
+            Controls.Add(txtNumFormatado);
+            Controls.Add(txtNumBruto);
+            Name = "frmDemo";
+            Text = "frmDemo";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtNumBruto;
+        private TextBox txtNumFormatado;
+        private Button btnFormatar;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtNumFormatado2;
+    }
+}
---
+++ PrjBancoConta/Demo/frmDemo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization; //biblioteca parar trabalhar com intercionalização
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolTip;
+
+namespace PrjBancoConta.Forms.Demo
+{
+    public partial class frmDemo : Form
+    {
+        public frmDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFormatar_Click(object sender, EventArgs e)
+        {
+            //Em C#, o sufixo M é usado para indicar que um valor numérico é do tipo decimal.
+            //decimal preco = 199.99;
+
+            //decimal preco = 199;
+
+            //decimal preco = 199 *(decimal) Math.PI;
+
+            //txtNumFormatado.Text = $"Preço do produto {preco:C}";//C: formatar em dinheiro dependedo da localidade
+
+            //txtNumFormatado.Text = $"Preço do produto {preco:N1}"; // quantidade de casas decimais 
+
+            //txtNumFormatado2.Text = $"Preço do produto {preco:N5}"; // quantidade de casas decimais
+
+
+
+            //double fatorPercentual = .70; //Como o valor de fatorPercentual é 0.70, o método ToString("P") o multiplica por 100 e adiciona o símbolo de porcentagem(%), resultando em 70 %.
+
+            /* txtNumFormatado.Text = $"Percentual: {fatorPercentual.ToString("P")}";*/ //O método ToString("P") converte o valor da variável fatorPercentual para uma string formatada como porcentagem.
+
+            //Manipulando a localização do sistema para inglês/Americano
+
+            //CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-UK"); //CultureInfo: é uma classe usada para fornecer informações relacionadas a uma localização específica, como formatação de data, hora. New cria uma nova instância da classe CultureInfo configurada/convenções  para a cultura Inglês (Reino Unido). O identificador "en-UK"
+           // decimal preco = 1999 * (decimal)Math.PI;
+            //txtNumFormatado.Text = $"Preço do produto {preco:C}";
+
+            //DateTime dataHoje = DateTime.Now;
+            //txtNumFormatado2.Text = dataHoje.ToString();
+
+
+            //Manipulando a saída
+            //txtNumFormatado2.Text =  preco.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR"));
+
+            //tipo long é um "int" com maior capacidade numerica.
+            //long cpf = 12345677788;
+            //txtNumFormatado2.Text = cpf.ToString("000.000.000-00");
+
+            // barra escapa o formato
+            DateTime data = DateTime.Now;
+            txtNumFormatado2.Text = data.ToString("dd \\de MMMM \\de \\ yyyy HH:mm");
+            
+            /*d - dia
+             * M - mes
+             * y - ano
+             * h - hora
+             * m - minuto
+             * s - segundo
+             */
+            
+        }
+    }
+}
